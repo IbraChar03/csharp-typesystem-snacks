fdb4cf5ee1d0d414fb8f63378301f3bb2b3dd51b
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-05 15:10:00 +0200

    snack 7

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -38,7 +38,7 @@
 //    Console.Write($"Inserisci il {i+1} numero  : ");
 //    int num = Convert.ToInt32(Console.ReadLine());
 //    somma += num;
-    
+
 //}
 //Console.Write($"La somma dei numeri è : {somma}");
 
@@ -51,13 +51,45 @@
 //    somma += i;
 //}
 //Console.WriteLine($"La somma dei numeri è : {somma}");
-//Console.WriteLine($"La media dei numeri è : {somma/9}");
+//Console.WriteLine($"La media dei numeri è : {somma / 9}");
 
 //snack 5
 
-Console.Write("Inserisc un numero : ");
-int numero = Convert.ToInt32(Console.ReadLine());
-if (numero % 2 == 0)
-    Console.WriteLine($"{numero}");
-else
-    Console.WriteLine($"{numero + 1}");
+//Console.Write("Inserisc un numero : ");
+//int numero = Convert.ToInt32(Console.ReadLine());
+//if (numero % 2 == 0)
+//    Console.WriteLine($"{numero}");
+//else
+//    Console.WriteLine($"{numero + 1}");
+
+
+//snack 6
+
+//string[] arrayInvitati = { "Luca", "Antonio", "Mario", "Luigi", "Paolo", "Alessandro" };
+//Console.Write("Inserisci il tuo nome : ");
+//string nome = Console.ReadLine();
+
+//if (nome == "" || nome == null)
+
+//    Console.WriteLine("Devi inserire un nome valido");
+
+//if (arrayInvitati.Contains(nome))
+//    Console.WriteLine("Sei stato invitato alla festa");
+
+//else
+//    Console.WriteLine("Non sei stato invitato alla festa");
+
+// snack 7
+
+int[] array = new int[6];
+for(var i = 0; i< 6; i++)
+{
+    Console.Write($"Inserisci il {i + 1} numero : ");
+   int num = Convert.ToInt32( Console.ReadLine());
+    if (num % 2 != 0)
+        array[i] = num;
+}
+for( var i = 0; i< array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
